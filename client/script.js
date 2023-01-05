0ebc2173e6b34d564f1dfc18b6ff5d42e3d70556
decktech <mdecker113@gmail.com>
2023-01-05 18:52:57 -0500

    connect client and server

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ function  loader(element) {
   loadInterval = setInterval(() => {
     element.textContent += '.'
 
-    if (element.textContent === '...') {
+    if (element.textContent === '....') {
       element.textContent = ''
     }
   }, 300)
@@ -50,9 +50,66 @@ function chatStripe(isAi, value, uniqueId) {
               alt="${isAi ? 'bot' : 'user'}"  
             />
           </div>
-          <div> class="message"</div>
+          <div class="message" id=${uniqueId}>${value}</div>
         </div>
       </div>
     `
   )
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  form.reset()
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, "", uniqueId)
+
+  chatContainer.scrollTop = chatContainer.scrollHeight
+
+  const messageDiv = document.getElementById(uniqueId)
+
+  loader(messageDiv)
+
+  // fetch data from server --> bot's response
+  const response = await fetch('http://localhost:3000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ''
+
+  if(response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+
+    console.log({parsedData})
+
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something went wrong"
+
+    alert(err)
+  }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e)
+  }
+})
